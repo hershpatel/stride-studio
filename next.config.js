4b27411794b38cc8864872e177cbaabff99ac320
Hersh 🍫 <phersh05@gmail.com>
2024-12-03 23:37:27 -0500

    probably not that secure but photo

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.cloudfront.net',  // Allow all Cloudfront subdomains
+      },
+    ],
+  },
+};
 
 export default config;
